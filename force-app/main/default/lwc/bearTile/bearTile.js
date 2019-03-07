08345d45aa94f11d2e4b3dbb5833b226d4893291
Justin Lyon <jml6487@gmail.com>
2019-03-07 07:41:03 -0600

    add clickable bear

--- force-app/main/default/lwc/bearTile/bearTile.js
+++ force-app/main/default/lwc/bearTile/bearTile.js
@@ -5,4 +5,11 @@ export default class BearTile extends LightningElement {
   appResources = {
     bearSilhouette: ursusResources + '/img/standing-bear-silhouette.png',
   };
+
+  handleOpenRecordClick() {
+    const selectEvent = new CustomEvent('bearview', {
+      bubbles: true
+    });
+    this.dispatchEvent(selectEvent);
+  }
 }
